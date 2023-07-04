af8db03b90b83aa100d10d95ea7a9ca004820d13
Brij Bhushan Maurya <brijbhushan9083@gmail.com>
2023-07-05 00:43:22 +0530

    Updated some Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -44,8 +44,8 @@ const Navbar = () => {
         >
           <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
           <p className='text-white text-[18px] font-bold cursor-pointer flex '>
-            Hasan &nbsp;
-            <span className='sm:block hidden'> | Full Stack Developer</span>
+            Hi, &nbsp;
+            <span className='sm:block hidden'>Brij Here!</span>
           </p>
         </Link>
 
